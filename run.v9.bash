13b33208f69c202e9c53dd3e347a7e70c8898ea3
Arnol Fokam <arnollfokam23@gmail.com>
2023-10-06 07:01:26 +0200

    update run script

--- run.v9.bash
+++ run.v9.bash
@@ -2,7 +2,7 @@
 
 # Define your array of models
 models=(
-    # "xcit_nano_12_p16_224"
+    "xcit_nano_12_p16_224"
     "xcit_nano_12_p8_224"
     "xcit_tiny_12_p16_224"
     "xcit_tiny_12_p8_224"
@@ -15,11 +15,10 @@ models=(
 )
 
 # Initialize an incrementing index
-index=2
+index=1
 
 # Loop through the array and call the Python program
 for model_name in "${models[@]}"; do
-    echo $model_name
     CUDA_VISIBLE_DEVICES=1 python solutions/v9/solution.py --index "#$index" --model_name "$model_name"
     ((index++))
 done
