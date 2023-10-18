df879b91a51e8223be062891520734a5deb01830
Arnol Fokam <arnollfokam23@gmail.com>
2023-10-18 04:58:51 +0200

    remove target transform

--- run.bash
+++ run.bash
@@ -15,10 +15,10 @@ index=1
 python setup.py install
 
 # run old model
-CUDA_VISIBLE_DEVICES=0 echo python solutions/v10/solution.py --index "#1" --model_name "$model_name"
+CUDA_VISIBLE_DEVICES=0 python solutions/v10/solution.py --index "#3" --model_name "$model_name"
 
-# Loop through the list and call the Python program
-for transformation in "${transformations[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 echo python solutions/v12/solution.py --index "#$index" --model_name "$model_name" --label_transform "$transformation"
-    ((index++))
-done
+# # Loop through the list and call the Python program
+# for transformation in "${transformations[@]}"; do
+#     CUDA_VISIBLE_DEVICES=0 python solutions/v12/solution.py --index "#$index" --model_name "$model_name" --label_transform "$transformation"
+#     ((index++))
+# done
